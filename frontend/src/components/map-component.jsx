6b9cd43d7c921e9e68724ded862938fa91e7d096
Guan Yi Lian <glia0011@student.monash.edu>
2025-09-23 17:47:41 +1000

    - Added initial coding for the map interactions, non-working yet

--- frontend/src/components/map-component.jsx
+++ frontend/src/components/map-component.jsx
@@ -1,5 +1,9 @@
 export default function MapComponent({ API_KEY }) {
     let map;
+    let originMarker = null;
+    let destinationMarker = null;
+    let directionsRenderer = null;
+
 
     function initMap() {
         const center = { lat: -34.397, lng: 150.644 };
@@ -10,6 +14,58 @@ export default function MapComponent({ API_KEY }) {
 
         const trafficLayer = new google.maps.TrafficLayer();
         trafficLayer.setMap(map);
+
+        directionsRenderer = new google.maps.DirectionsRenderer();
+        directionsRenderer.setMap(map);
+
+        map.addListener("click", (e) =>{
+            const clicked = { lat: e.latLng.lat(), lng: e.latLng.lng() };
+
+            if (!originMarker){
+                originMarker = new google.maps.marker.AdvancedMarkerElement({
+                    map: map,
+                    position: clicked,
+                    title:"A",
+                });
+                directionsRenderer.set("directions",null);
+            }else if (!destinationMarker){
+                destinationMarker = new google.maps.marker.AdvancedMarkerElement({
+                    map: map,
+                    position: clicked,
+                    title:"B",
+                });
+                buildRoute(originMarker.position,destinationMarker.position);
+        } else{
+            originMarker.map = null;
+            destinationMarker.map =null;
+            directionsRenderer.set("directions",null);
+
+            originMarker = new google.maps.marker.AdvancedMarkerElement({
+                    map: map,
+                    position: clicked,
+                    title:"A",
+                });
+                destinationMarker = null;
+        }
+    });
+    }
+
+    function buildRoute(origin,destination){
+        const directionsService = new google.maps.DirectionsService();
+        directionsService.route(
+            {
+                origin,
+                destination,
+                travelMode: google.maps.TravelMode.DRIVING,
+            },
+            (result, status) =>{
+                if (status === "OK"){
+                    directionsRenderer.setDirections(result) 
+                } else{
+                    console.error("Direction request failed:" + status)
+                }
+            }
+        )
     }
 
     window.initMap = initMap;
