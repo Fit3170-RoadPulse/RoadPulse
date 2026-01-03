6f8193010aab37a7d402739550fd10f37cbb8253
Danny Yan <yandanny2004@gmail.com>
2026-01-03 13:58:44 +1100

    Minor fixed to naming

--- frontend/src/pages/Settings/ChangePassword/ChangePassword.jsx
+++ frontend/src/pages/Settings/ChangePassword/ChangePassword.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import SettingMenu from "../menu/SettingMenu";
+import SettingMenu from "../Menu/SettingMenu";
 import CloseIcon from "../../../assets/close.png";
 import "./ChangePassword.css";
 
