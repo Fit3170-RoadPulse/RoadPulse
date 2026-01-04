a20555d18a2b6d80a8c767f6afbe45bcf84cdfea
xwon0021 <xwon0021@student.monash.edu>
2026-01-05 01:32:42 +0800

    Added admin page

---
+++ frontend-admin/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <div className="content">
+        <h1 className="title">RoadPulse Admin Panel</h1>
+        <p className="description">Administrative dashboard for RoadPulse</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
